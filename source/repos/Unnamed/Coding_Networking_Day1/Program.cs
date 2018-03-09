c2c9e7b898f50624763d5e619ebfca598f349ef4
QuangNguyenTheChick <quangkaka0702@gmail.com>
2018-03-09 16:38:04 +0700

    Let it go!

---
+++ source/repos/Unnamed/Coding_Networking_Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Networking_Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello God Damn Fucking World !!!");
+        }
+    }
+}
